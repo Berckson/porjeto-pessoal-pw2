d603626bbf4979de20ae7c7db6122996c88e492d
Berckson Thierry <abreuthierry3@gmail.com>
2025-06-17 08:18:40 -0300

    Update ListaRealizada.jsx

--- src/Components/ListaRealizada.jsx
+++ src/Components/ListaRealizada.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ListaRealizada({ comentarios }) {
   return (
     <div className="comentarios">
-      <h3>Frequêcia:</h3>
+      <h3 className="h3">Frequêcia:</h3>
       <ul>
         {comentarios.map((comentario, index) => (
           <li key={index}>
